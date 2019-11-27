80f02101a676bdfb731353ae8027786969e169ec
Sam Mitchell <samuel.alan.mitchell@gmail.com>
2019-11-27 10:26:31 -0500

    updated readme for teaching

--- scripts/control.py
+++ scripts/control.py
@@ -11,46 +11,75 @@ class Control:
         rospy.init_node('control', anonymous=True)
 
         # Choose the method to control the vehicle
-        control_method = 'fixed'
-        if control_method == 'fixed':
+        control_method = 'circle'
+        if control_method == 'circle':
+            self.circle_control()
+        elif control_method == 'fixed':
             rospy.Subscriber('/line_position', Float32, self.fixed_number_control)
         elif control_method == 'proportional':
             rospy.Subscriber('/line_position', Float32, self.proporional_control)
 
-    def fixed_number_control(self, msg):
-        line_center = msg.data
-        vel_command = 0.2
 
-        line_threshold = 0.030
-        turn_magnitude = 0.7
+    def circle_control(self):
+        # Can we make the vehicle go faster or slower in a straight line?
+        # Can we make the vehicle turn left or right? 
+        # How sharp can the vehicle turn?
+        vel_command = 0.2
+        curv_command = -1.0
+
+        while not rospy.is_shutdown():
+            self.curvature_pub.publish(curv_command)
+            self.vel_pub.publish(vel_command)
+            rospy.sleep(0.1)
+ 
+
+    def fixed_number_control(self, msg):
+        # Every time the line is measured, it sends a message with how far to
+        # the side the detected line is. We can use that information to make
+        # the robot react to the line.
+        line_center = msg.data
+
+        # Set up a way to turn if the vehicle is off the path. 
+        # Use 3 types of reactions:
+        #   1. The line is centered.
+        #   2. The line is to the right.
+        #   3. The line is to the left.
+
+        # How far off does the line need to be in order to react?
+        line_threshold = 0.1
         go_straight = 0.0
 
-        # use line center to choose to turn left, right, or go straight.
+        if abs(line_center) < line_threshold:
+            curv_command = go_straight
 
-        curv_command = go_straight
+        elif line_center > line_threshold:
+            # Change this value to make the turtlebot respond
+            curv_command = 0.0
 
-        # # TODO remove this.
-        # if line_center > line_threshold:
-        #     # turn right if line is to the left
-        #     curv_command = -1 * turn_magnitude
-
-        # elif line_center < -1 * line_threshold:
-        #     # turn left if the line is to the right
-        #     curv_command = turn_magnitude
-
-        # else:
-        #     curv_command = go_straight
+        elif line_center < -1 * line_threshold:
+            # Change this value to make the turtlebot respond
+            curv_command = 0.0
 
         self.curvature_pub.publish(curv_command)
+
+        vel_command = 0.1
         self.vel_pub.publish(vel_command)
 
     def proportional_control(self,msg):
         line_center = msg.data
-        vel_command = 0.2
+        vel_command = 0.1
         curv_command = 0.0
 
-        # how would we calculate a curvature command using a proportional
-        # equation (ratio)?
+        # Turning by a fixed magnitude makes the control jittery.
+        # We can do better.
+
+        # Right now, we turn left or right by a fixed amount. Instead we could
+        # turn left a little bit if the line is a little right, and a lot left
+        # if the line is very right.
+
+        # Come up with an equation that would allow for smoother turn control
+        # of the robot. 
+        # Hint. The equation might look like y=m*x+b.
 
         self.curvature_pub.publish(curv_command)
         self.vel_pub.publish(vel_command)
